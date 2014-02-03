21f0cb3fec5707e1d7a950460b209af94ec2a54a
Sandeep Puri <lexlapax@gmail.com>
2014-02-02 23:21:09 -0800

    Update buildimage.sh

--- tiny-haproxy/buildimage.sh
+++ tiny-haproxy/buildimage.sh
@@ -15,6 +15,23 @@ cp Dockerfiles/tiny-haproxy/buildroot.config buildroot-$BUILDROOTVER/.config
 cd buildroot-$BUILDROOTVER
 make all
 # wait a really long time while it builds everything including the toolchain
+# 
+
+
+# quoted from the docker buildroot blog post
+#
+# You should see a small, lean, rootfs.tar file, containing the image to be imported 
+# in Docker. But it’s not quite ready yet. We need to fix a few things. 
+# 
+# Docker sets the DNS configuration by bind-mounting over /etc/resolv.conf. 
+# This means that /etc/resolv.conf has to be a standard file. By default, 
+# buildroot makes it a symlink. We have to replace that 
+# symlink with a file (an empty file will do). 
+# 
+# Likewise, Docker “injects” itself within containers by bind-mounting over /sbin/init. 
+# This means that /sbin/init should be a regular file as well. By default, 
+# buildroot makes it a symlink to busybox. We will change that, too.
+
 mkdir -p output/images/fixup/sbin output/images/fixup/etc output/images/fixup/share/haproxy
 cp -r output/build/haproxy-$HAPROXY_VERSION/examples output/images/fixup/share/haproxy/
 cp -r output/build/haproxy-$HAPROXY_VERSION/VER* output/images/fixup/share/haproxy/
