9a24f6c3abf41a578241016db4bdfb1eeda8e90e
Lex Lapax <lexlapax@gmail.com>
2014-02-03 20:51:46 -0800

    added tiny-mosquitto

---
+++ tiny-mosquitto/buildroot/package/mosquitto/mosquitto.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# mosquitto
+#
+################################################################################
+
+MOSQUITTO_VERSION = 1.2.3
+MOSQUITTO_SOURCE = mosquitto-$(MOSQUITTO_VERSION).tar.gz
+MOSQUITTO_SITE = http://mosquitto.org/files/source/$(MOSQUITTO_SOURCE)
+MOSQUITTO_DEPENDENCIES = openssl zlib
+MOSQUITTO_CONF_OPT = -DCMAKE_INSTALL_PREFIX=/usr
+
+
+define MOSQUITTO_USERS
+	mosquitto -1 mosquitto -1 * - - - mosquitto mqtt daemon
+endef
+
+$(eval $(cmake-package))
