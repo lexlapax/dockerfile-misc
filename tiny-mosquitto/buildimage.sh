c7128acf7ca5a5f96e3377b0cdce445cbb873eee
Andus Lim <yuncai83@gmail.com>
2015-01-15 18:57:27 +0800

    Updated tiny-mosquitto buildimage.sh script to fix invalid path and updated buildroot version to 14.11

--- tiny-mosquitto/buildimage.sh
+++ tiny-mosquitto/buildimage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export IMAGENAME=tinymosquitto
-export BUILDROOTVER=2013.11
+export BUILDROOTVER=2014.11
 
 
 mkdir ~/src
@@ -9,20 +9,20 @@ cd ~/src
 wget http://buildroot.uclibc.org/downloads/buildroot-$BUILDROOTVER.tar.gz
 tar -xzvf buildroot-$BUILDROOTVER.tar.gz
 git clone https://github.com/lexlapax/Dockerfiles
-cp -r Dockerfiles/mosquitto-lite/package/mosquito buildroot-$BUILDROOTVER/package/
-cp Dockerfiles/mosquitto-lite/buildroot.config buildroot-$BUILDROOTVER/.config
+cp -r Dockerfiles/tiny-mosquitto/buildroot/package/mosquitto buildroot-$BUILDROOTVER/package/
+cp Dockerfiles/tiny-mosquitto/buildroot/buildroot.config buildroot-$BUILDROOTVER/.config
 cd buildroot-$BUILDROOTVER
 make all
 # wait a really long time while it builds everything including the toolchain
 cd output/images
 mkdir extra extra/sbin extra/etc
 touch extra/sbin/init extra/etc/resolv.conf
-cp root.tar fixup.tar
+cp rootfs.tar fixup.tar
 tar rvf fixup.tar -C extra .
 
 # docker steps
-docker import - tinyimage < fixup.tar
-cd ../../../Dockerfiles/mosquitto-lite
+sudo docker import - tinyimage < fixup.tar
+cd ../../../Dockerfiles/tiny-mosquitto
 
 sed -i.bak -e "s/FROM lapax\/buildroot:mosquitto/FROM tinyimage/g" Dockerfile
 sudo docker build -t $IMAGENAME .
